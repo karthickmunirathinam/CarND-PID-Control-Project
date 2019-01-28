7c931f3c56da8869e103bfbb8a08a2c4ad4c65a7
karthickmunirathinam <timmy.2008@gmail.com>
2019-01-28 09:46:16 +0100

    final commit with video compressed

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.25, 0.003, 3);
+  pid.Init(0.25, 0.00, 3);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
